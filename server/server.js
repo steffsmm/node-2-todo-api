2de5d184677934ba4ef0960558ad5ac7435adf1e
Стефани Мучинова <you@example.com>
2017-10-24 21:19:38 +0300

    Adding a PATCH part

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var express    = require('express')
-var bodyParser = require('body-parser')
-var app = express()
+
+const _ = require('lodash');
+const express    = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
 //node-todo-api-database
 // parse application/x-www-form-urlencoded
@@ -65,6 +67,27 @@ app.delete("/Todos/:id",(req,res)=>{
     res.status(200).send({todo});
 
   }).catch((e)=>res.status(400).send(e))
+
+})
+
+app.patch("/Todos/:id",(req,res)=>{
+  var id = req.params.id
+  var body = _.pick(req.body,["text","completed"])
+  if (!ObjectID.isValid(id)){
+    return res.status(404)
+  }
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+    if (!todo){return res.status(404).send()}
+    res.send({todo});
+
+  }).catch((e)=>res.status(400).send())
 
 })
 
