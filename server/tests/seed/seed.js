3787fbb58058cedf7a6de8cec23aca532d617164
Стефани Мучинова <you@example.com>
2017-12-27 18:05:35 +0200

    Add test on DELETE users/me/token

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -38,12 +38,13 @@ const populateTodos = (done)=>{
   .then(()=>done());
 }
 
-const populateUsers = (done)=>{
-  User.remove({}).then(()=>{
-    var userOne = new User(users[0]).save()
-    var userTwo = new User(users[1]).save()
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
     return Promise.all([userOne, userTwo])
-  }).then(()=>done())
-}
+  }).then(() => done());
+};
 
 module.exports = {todos,populateTodos,users,populateUsers};
